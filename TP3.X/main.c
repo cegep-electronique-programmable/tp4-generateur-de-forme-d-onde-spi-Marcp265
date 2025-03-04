9a918ecf994b0b66da813f3f7ff388dae3b75f0b
e1779425 <e1779425@P310A-12.cmaisonneuve.qc.ca>
2025-03-04 11:59:32 -0500

    Deccommente le code

--- TP3.X/main.c
+++ TP3.X/main.c
@@ -3,7 +3,7 @@
  * Maxime Champagne
  * 3 mars 2022
  * 
- * Modifié par
+ * Modifié par Marc Pilon
  *
  *
  * SPI/main.c
@@ -39,7 +39,7 @@ uint8_t const tri[MAX] ={
 
 void out_dig(uint8_t x);
 void sinus_60(void);
-//void myTimer1_ISR(void);
+void myTimer1_ISR(void);
 
 /*
                          Main application
@@ -51,11 +51,11 @@ void main(void)
     
     SYSTEM_Initialize();
     
-    //INTERRUPT_GlobalInterruptEnable();
+    INTERRUPT_GlobalInterruptEnable();
     
-    //INTERRUPT_PeripheralInterruptEnable();
+    INTERRUPT_PeripheralInterruptEnable();
     
-    //TMR1_SetInterruptHandler(myTimer1_ISR);
+    TMR1_SetInterruptHandler(myTimer1_ISR);
 
     SSPCON1bits.SSPEN = 1;
     IO_RA5_SetHigh();
@@ -64,36 +64,48 @@ void main(void)
     {
         
         
-//        //Code de test pour valider le fonctionnement du potentiomètre
-//        {
-//            printf("\n\rEntrer une valeur entre 0 et 255, suivie de [Enter]");
-//            valeur = 0;
-//            do
+        //Code de test pour valider le fonctionnement du potentiomètre
+        {
+            printf("\n\rEntrer une valeur entre 0 et 255, suivie de [Enter]");
+            valeur = 0;
+            do
+            {
+                do
+                {
+                    lecture = EUSART1_Read();               
+                }
+                while (((lecture < '0') || (lecture > '9')) && (lecture != 0x0d));
+                if ((lecture >= '0') && (lecture <= '9')) 
+                {
+                    valeur = 10 * valeur + lecture - '0';
+                    putchar(lecture);
+                }
+            }
+        
+            while ((lecture != 0x0d) && (valeur < 26)); 
+            tension = (float)5* valeur /256;
+            printf("\n\rValeur = %u tension = %3.2f ", valeur, tension);
+            out_dig(valeur);    // envoi sur potentiometre
+            
+//            switch(EUSART_Read())
 //            {
-//                do
-//                {
-//                    if(EUSART1_is_rx_ready()){
-//                        lecture = EUSART1_Read();
-//                    }                    
-//                }
-//                while (((lecture < '0') || (lecture > '9')) && (lecture != 0x0d));
-//                if ((lecture >= '0') && (lecture <= '9')) 
-//                {
-//                    valeur = 10 * valeur + lecture - '0';
-//                    putchar(lecture);
-//                }
+//                case 's':
+//                    sinus_60;
+//                    break;
+//                case 'c':
+//                    carre_60;
+//                    break;
+//                case 't':
+//                    triangle_60;
+//                    break;
+//                default:
+//                    printf("\n\rNot Onde");
 //            }
-//        
-//            while ((lecture != 0x0d) && (valeur < 26)); 
-//            tension = (float)5* valeur /256;
-//            printf("\n\rValeur = %u tension = %3.2f ", valeur, tension);
-//            out_dig(valeur);    // envoi sur potentiometre 
-//        } 
-        
-        
+            
+        }
         
         //Code de test pour générer une onde sinusoidale
-        sinus_60();
+        //sinus_60(); //change voltage
         
     }
 
@@ -103,18 +115,18 @@ void main(void)
 //---------------------------------------------------------------
 // Routine d'interruption du Timer1
 //---------------------------------------------------------------
-//void myTimer1_ISR(void){
-//    static uint8_t i; 
-//    
-//    TMR1_WriteTimer(0x3456);
-//    
-//    out_dig(sin[i]);
-//    
-//    i++;
-//    if (i==MAX){
-//        i=0;
-//    }
-//}
+void myTimer1_ISR(void){
+    static uint8_t i; 
+    
+    TMR1_WriteTimer(0x3456);
+    
+    out_dig(sin[i]);
+    
+    i++;
+    if (i==MAX){
+        i=0;
+    }
+}
     
 //----------------------------------------------------------------
 // Transmission au pot. d'une onde comprenant 60 points par cycle.
